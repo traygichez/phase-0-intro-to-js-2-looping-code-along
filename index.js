bf061989fd429cb7288b93c981d6846220f0c294
tray <onesimus.githua@student.moringaschool.com>
2023-09-22 20:33:56 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solutions in this file
+function writeCards(names,eventName){
+          const thankYouMessages=[]; for(let a=0;a< names.length;a++){
+                  const name = names[a];
+                        const message = `Thank you, ${name}, for the wonderful ${eventName} gift!`;
+                                    thankYouMessages.push(message); 
+    } return thankYouMessages;
+}
+function countDown()
+{ let countDown=10;
+          while (countDown >= 0) {
+                        console.log(countDown--);
+}countDown;
+}
